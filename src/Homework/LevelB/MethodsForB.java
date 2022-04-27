2ce5e05b5af8a5ad4886e00bb54acc155d0580b0
Anastasia <aurummiak@gmail.com>
2022-04-27 23:10:21 +0300

    Saved homework [Level B].

---
+++ src/Homework/LevelB/MethodsForB.java
@@ -0,0 +1,121 @@
+package Homework.LevelB;
+
+import java.util.Random;
+
+public class MethodsForB {
+    public static void randomElementsForArray(int[] array, int size, int min, int max) {
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(max - min + 1) + min;
+        }
+    }
+
+    public static String findingLargerElement(int[] array, int size, int number) {
+        String res = "";
+        int count = 0;
+        for (int i : array) {
+            if (i > number) {
+                count++;
+            }
+        }
+        res = String.valueOf(count);
+        return res;
+    }
+
+    public static String findingLessElement(int[] array, int size, int number) {
+        String res = "";
+        int count = 0;
+        for (int i : array) {
+            if (i < number) {
+                count++;
+            }
+        }
+        res = String.valueOf(count);
+        return res;
+    }
+
+    public static String findingEqualElement(int[] array, int size, int number) {
+        String res = "";
+        int count = 0;
+        for (int i : array) {
+            if (i == number) {
+                count++;
+            }
+        }
+        res = String.valueOf(count);
+        return res;
+    }
+
+    public static String findingNotEqualElement(int[] array, int size, int number) {
+        String res = "";
+        int count = 0;
+        for (int i : array) {
+            if (i != number) {
+                count++;
+            }
+        }
+        res = String.valueOf(count);
+        return res;
+    }
+
+    public static String findingMultipleElement(int[] array, int size, int number) {
+        String res = "";
+        int count = 0;
+        for (int i : array) {
+            if (i % number == 0 && i != 0) {
+                count++;
+            }
+        }
+        res = String.valueOf(count);
+        return res;
+    }
+
+    public static String findingNotMultipleElement(int[] array, int size, int number) {
+        String res = "";
+        int count = 0;
+        for (int i : array) {
+            if (i % number != 0 && i != 0) {
+                count++;
+            }
+        }
+        res = String.valueOf(count);
+        return res;
+    }
+
+    public static String equalModuleElement(int[] array, int size, int number) {
+        String res = "";
+        int count = 0;
+        for (int i : array) {
+            if (i == Math.abs(number)) {
+                count++;
+            }
+        }
+        res = String.valueOf(count);
+        return res;
+    }
+
+    public static String largerModuleElement(int[] array, int size, int number) {
+        String res = "";
+        int count = 0;
+        for (int i : array) {
+            if (i > Math.abs(number)) {
+                count++;
+            }
+        }
+        res = String.valueOf(count);
+        return res;
+    }
+
+    public static String lessModuleElement(int[] array, int size, int number) {
+        String res = "";
+        int count = 0;
+        for (int i : array) {
+            if (i < Math.abs(number)) {
+                count++;
+            }
+        }
+        res = String.valueOf(count);
+        return res;
+    }
+
+}
---
+++ src/Homework/LevelB/task3/Main.java
@@ -0,0 +1,27 @@
+package Homework.LevelB.task3;
+
+import Homework.LevelB.MethodsForB;
+import Homework.view.Printer;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Printer.print("\nEnter the size of array: ");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+
+        MethodsForB.randomElementsForArray(array, size, -10, 10);
+        Printer.print("Array: " + Arrays.toString(array));
+
+
+        Printer.print("\nEnter the number: ");
+        int number = scanner.nextInt();
+        Printer.print("Quantity of elements, which are equal: " + MethodsForB.findingEqualElement(array, size, number));
+
+    }
+}
+
