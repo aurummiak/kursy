80929b9d3b0ff62b6e5b0f17025c681a08134470
Anastasia <aurummiak@gmail.com>
2022-04-29 23:56:50 +0300

    Saved homework [Level D].

---
+++ src/Homework/LevelD/task1/Main.java
@@ -0,0 +1,23 @@
+package Homework.LevelD.task1;
+
+import Homework.LevelD.MethodsForD;
+import view.Printer;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Printer.print("\nEnter the size of array: ");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+
+        MethodsForD.randomElementsForArray(array, size, 0, 5);
+        Printer.print("Array: " + Arrays.toString(array));
+
+        Printer.print("\nResult: " + MethodsForD.differentValues(array,size));
+
+    }
+}
---
+++ src/Homework/LevelD/task3/Main.java
@@ -0,0 +1,23 @@
+package Homework.LevelD.task3;
+
+import Homework.LevelD.MethodsForD;
+import view.Printer;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Printer.print("\nEnter the size of array: ");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+
+        MethodsForD.randomElementsForArray(array, size, -10, 10);
+        Printer.print("Array: " + Arrays.toString(array));
+
+        Printer.print("\nResult: " + MethodsForD.differentCoupleValues(array, size));
+
+    }
+}
